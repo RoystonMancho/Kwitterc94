6815d203c5d71c4ed8e5191253344c6baf5b9c18
RoystonMancho <87150444+RoystonMancho@users.noreply.github.com>
2022-01-01 14:08:53 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAEJDqpe0VKLCys43gQ5sSKH1bikD-BMcw",
+    authDomain: "project-94-29084.firebaseapp.com",
+    databaseURL: "https://project-94-29084-default-rtdb.firebaseio.com",
+    projectId: "project-94-29084",
+    storageBucket: "project-94-29084.appspot.com",
+    messagingSenderId: "956202386687",
+    appId: "1:956202386687:web:d743c4c826f3c5c3d5b8ca"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
